3d0b2f7e084eae486da2efb186660dfaf11bf832
gabrielly reis <gabyreis88@gamil.com>
2025-04-05 19:50:10 -0300

    Remove redirecionamento automático para a página inicial no carregamento

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,6 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-if (window.location.pathname !== "/home") {
-  window.history.replaceState("/home", "", "/home");
-  window.location.reload();
-}
-
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
